c64edfd44ebb291a6ea276e544095fb2051a13c6
iAtog <atogmc58vani@gmail.com>
2022-02-04 23:01:15 -0400

    chore(nms): the code has been reduced

--- crossbow-1.12.2/src/main/java/me/iatog/crossbow/nms/v1_12_2/CraftNMSv1_12_2.java
+++ crossbow-1.12.2/src/main/java/me/iatog/crossbow/nms/v1_12_2/CraftNMSv1_12_2.java
@@ -7,11 +7,15 @@ import org.bukkit.inventory.ItemStack;
 import net.minecraft.server.v1_12_R1.NBTTagCompound;
 
 public class CraftNMSv1_12_2 implements CraftNMS {
-
+	
+	private NBTTagCompound getTag(net.minecraft.server.v1_12_R1.ItemStack craftItemStack) {
+		return craftItemStack.getTag() == null ? new NBTTagCompound() : craftItemStack.getTag();
+	}
+	
 	@Override
 	public ItemStack setNBTString(ItemStack item, String key, String value) {
 		net.minecraft.server.v1_12_R1.ItemStack craftItemStack = CraftItemStack.asNMSCopy(item);
-		NBTTagCompound tag = craftItemStack.getTag() == null ? new NBTTagCompound() : craftItemStack.getTag();
+		NBTTagCompound tag = getTag(craftItemStack);
 		
 		tag.setString(key, value);
 		craftItemStack.setTag(tag);
@@ -21,7 +25,7 @@ public class CraftNMSv1_12_2 implements CraftNMS {
 	@Override
 	public ItemStack setNBTBoolean(ItemStack item, String key, boolean value) {
 		net.minecraft.server.v1_12_R1.ItemStack craftItemStack = CraftItemStack.asNMSCopy(item);
-		NBTTagCompound tag = craftItemStack.getTag() == null ? new NBTTagCompound() : craftItemStack.getTag();
+		NBTTagCompound tag = getTag(craftItemStack);
 		
 		tag.setBoolean(key, value);
 		craftItemStack.setTag(tag);
@@ -31,7 +35,7 @@ public class CraftNMSv1_12_2 implements CraftNMS {
 	@Override
 	public boolean hasKey(ItemStack item, String key) {
 		net.minecraft.server.v1_12_R1.ItemStack craftItemStack = CraftItemStack.asNMSCopy(item);
-		NBTTagCompound tag = craftItemStack.getTag() == null ? new NBTTagCompound() : craftItemStack.getTag();
+		NBTTagCompound tag = getTag(craftItemStack);
 		
 		return tag.hasKey(key);
 	}
@@ -39,7 +43,7 @@ public class CraftNMSv1_12_2 implements CraftNMS {
 	@Override
 	public String getString(ItemStack item, String key) {
 		net.minecraft.server.v1_12_R1.ItemStack craftItemStack = CraftItemStack.asNMSCopy(item);
-		NBTTagCompound tag = craftItemStack.getTag() == null ? new NBTTagCompound() : craftItemStack.getTag();
+		NBTTagCompound tag = getTag(craftItemStack);
 		
 		return tag.getString(key);
 	}
@@ -47,7 +51,7 @@ public class CraftNMSv1_12_2 implements CraftNMS {
 	@Override
 	public boolean getBoolean(ItemStack item, String key) {
 		net.minecraft.server.v1_12_R1.ItemStack craftItemStack = CraftItemStack.asNMSCopy(item);
-		NBTTagCompound tag = craftItemStack.getTag() == null ? new NBTTagCompound() : craftItemStack.getTag();
+		NBTTagCompound tag = getTag(craftItemStack);
 		
 		return tag.getBoolean(key);
 	}
